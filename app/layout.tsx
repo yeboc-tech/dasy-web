02bed90559f817fb77fc8d4bda600b7568cb626a
Joon Nam <jungeol66104@gmail.com>
2025-08-12 05:42:20 +0900

    mid

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar";
 import "./globals.css";
 
 export const metadata: Metadata = {
